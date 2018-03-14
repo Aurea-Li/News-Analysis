6b3ebfe32903614ac9e2175dd7931d12ce14d73b
Aurea-Li <fu.sakura@gmail.com>
2018-03-13 20:22:41 -0700

    hide key

--- NewsAPI.py
+++ NewsAPI.py
@@ -1,13 +1,19 @@
 import requests, json
+from AUTH import news_key
 
 def printJSON(JSON):
 	''' Prints JSON object in readable format'''
 	print(json.dumps(JSON, indent=4))
 
-url = ('https://newsapi.org/v2/top-headlines?'
-       'country=us&'
-       'apiKey=19771d3100434feda08963940ac937f6')
+# url = ('https://newsapi.org/v2/top-headlines?'
+#        'country=us&'
+#        'apiKey=' + news_key)
 
-      
+url = ('https://newsapi.org/v2/everything?'
+       'q=rex tillerson fired&'
+       'language=en&'
+       'apiKey=' + news_key)
+
+
 response = requests.get(url)
 printJSON(response.json())
