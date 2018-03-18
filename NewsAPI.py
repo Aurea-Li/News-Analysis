6fb0e45b1751f6ffdf94453095066fa616974914
Aurea-Li <fu.sakura@gmail.com>
2018-03-18 13:45:50 -0700

    finish calculating average delay length, need to implement graph

--- NewsAPI.py
+++ NewsAPI.py
@@ -1,10 +1,14 @@
 import requests, json
+from datetime import datetime
 from paralleldots import similarity, ner, taxonomy, sentiment, keywords, intent, emotion, abuse
 from AUTH import news_key, pd_key
 
 def printJSON(JSON):
 	''' Prints JSON object in readable format'''
 	print(json.dumps(JSON, indent=4))
+
+def convertDatetime(datet):
+    return datetime.strptime(datet, '%Y-%m-%dT%H:%M:%SZ')
 
 
 # Get list of all english sources
@@ -36,12 +40,47 @@ url = ('https://newsapi.org/v2/everything?'
        'apiKey=' + news_key)
 response = requests.get(url).json()
 
+# Verify relevancy of articles
+
 # Create matrix of relevancy
 relevance = []
 
-# for article in response['articles']:
+# Publish time, publish time delay dicts
+publishtimes = {}
+publishdelay = {}
+test = {}
 
-printJSON(response)
+# Save each time
+for article in response['articles']:
+    publishtimes[article['source']['id']] = convertDatetime(article['publishedAt'])
+    test[article['source']['id']] = article['publishedAt']
+
+
+# Get time of earliest published article (time zero)
+time_zero = min(publishtimes.values())
+
+
+# Calculate delay time of every other article
+
+for source in publishtimes:
+    publishdelay[source] = (publishtimes[source] - time_zero).total_seconds() / 60
+
+print(publishdelay)
+print(test)
+
+# Plot bar graph
+
+# delta = publish_delay['cbs-news'][1] - publish_delay['cnn'][1]
+
+# rdelta = publish_delay['cnn'] - publish_delay['cbs-news']
+
+
+
+# print(min(publish_delay,key=publish_delay.get))
+
+
+
+
 
 
 
