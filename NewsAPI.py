52ea66ac7fffad53ac1eca326e81a99353a43211
Aurea-Li <fu.sakura@gmail.com>
2018-03-17 17:14:00 -0700

    adding paralleldots API

--- NewsAPI.py
+++ NewsAPI.py
@@ -1,19 +1,24 @@
 import requests, json
-from AUTH import news_key
+from paralleldots import similarity, ner, taxonomy, sentiment, keywords, intent, emotion, abuse
+from AUTH import news_key, pd_key
 
 def printJSON(JSON):
 	''' Prints JSON object in readable format'''
 	print(json.dumps(JSON, indent=4))
 
-# url = ('https://newsapi.org/v2/top-headlines?'
-#        'country=us&'
-#        'apiKey=' + news_key)
 
 url = ('https://newsapi.org/v2/everything?'
        'q=rex tillerson fired&'
        'language=en&'
+       'pagesize=100&'
        'apiKey=' + news_key)
 
 
-response = requests.get(url)
-printJSON(response.json())
+# Get list of articles
+response = requests.get(url).json()
+
+print(similarity("Sachin is the greatest batsman",
+           "Tendulkar is the finest cricketer"))
+
+# printJSON(response)
+
